6aff4d272f3bb8faaa6558ef3442b149dd017dd5
MoepyaCode <kagisomoepya19@gmail.com>
2024-10-13 21:42:49 +0200

    🎉 Project init tip-calculator-app

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import { Main } from "../../components"
+
+export default function Home() {
+
+  return (
+    <Main className="bg-slate-400 grid place-items-center ">
+        Hello World
+    </Main>
+  )
+}
